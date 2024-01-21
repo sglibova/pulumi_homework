50a80667f5bced4a54aa4da141a7273b370d3c13
Svitlana Glibova <s.glibova@gmail.com>
2024-01-21 15:49:06 -0800

    add image and image registry

--- web_server/__main__.py
+++ web_server/__main__.py
@@ -1,12 +1,14 @@
 import os
 import pulumi
 import pulumi_docker as docker
+import pulumi_aws as aws
 import pulumi_awsx as awsx
 import pulumi_eks as eks
+import pulumi_kubernetes as k8s
 
 from pulumi import ComponentResource
 
-
+# TODO: Encapsulate the below resources
 class EKSCluster(ComponentResource):
     def __init__(self):
         return None
@@ -14,26 +16,41 @@ class EKSCluster(ComponentResource):
 
 # Get some values from the Pulumi configuration (or use defaults)
 config = pulumi.Config()
+
+# Image Config
+config_color = config.get("config_color", "red")
+# ECR Config
+ecr_user=config.get("ecrUser")
+ecr_repo_name = "web_server_repo"
+
+# EKS Config
 min_cluster_size = config.get_float("minClusterSize", 3)
 max_cluster_size = config.get_float("maxClusterSize", 6)
 desired_cluster_size = config.get_float("desiredClusterSize", 3)
 eks_node_instance_type = config.get("eksNodeInstanceType", "t3.medium")
 vpc_network_cidr = config.get("vpcNetworkCidr", "10.0.0.0/16")
-config_color = config.get("config_color", "red")
 
 stack = pulumi.get_stack()
 
+# Create an ECR resource for the Docker image built above
+ecr_repo = aws.ecr.Repository(ecr_repo_name, name="docker-image-repo")
+auth_token = aws.ecr.get_authorization_token_output(registry_id=ecr_repo.registry_id)
+
 # Build backend Image
-backend_image_name="backend"
-backend = docker.Image("backend",
+backend_image_name="flask_server"
+backend_image = docker.Image("flask_server",
                         build=docker.DockerBuildArgs(
                             args={
+                                "platform":"linux/arm64",
                                 "env": f"CONFIG_COLOR={config_color}"
                             },
                             context=f"{os.getcwd()}/app"),
-                        image_name=f"{backend_image_name}:{stack}",
-                        skip_push=True
-                        )
+                        image_name=ecr_repo.repository_url.apply(lambda repository_url: f"{repository_url}:latest"),
+                        registry=docker.RegistryArgs(
+        username=ecr_user,
+        password=pulumi.Output.secret(auth_token.password),
+        server=ecr_repo.repository_url,
+        ))
 
 # Create a VPC for the EKS cluster
 eks_vpc = awsx.ec2.Vpc("eks-vpc",
@@ -59,6 +76,29 @@ eks_cluster = eks.Cluster("eks-cluster",
     endpoint_private_access=False,
     endpoint_public_access=True
     )
+
+# Create a Kubernetes Deployment
+app_labels = {'app': 'web-server'}
+deployment = k8s.apps.v1.Deployment('web-server-dep',
+                                    metadata=k8s.meta.v1.ObjectMetaArgs(labels=app_labels),
+                                    spec=k8s.apps.v1.DeploymentSpecArgs(
+                                        replicas=1,
+                                        selector=k8s.meta.v1.LabelSelectorArgs(match_labels=app_labels),
+                                        template=k8s.core.v1.PodTemplateSpecArgs(
+                                            metadata=k8s.meta.v1.ObjectMetaArgs(labels=app_labels),
+                                            spec=k8s.core.v1.PodSpecArgs(
+                                                containers=[
+                                                    k8s.core.v1.ContainerArgs(
+                                                        name='myapp',
+                                                        image=f'{ecr_repo.repository_url}:latest',
+                                                        ports=[k8s.core.v1.ContainerPortArgs(name='http', container_port=80)]
+                                                    )
+                                                ]
+                                            )
+                                        )
+                                    ),
+                                    opts=pulumi.ResourceOptions(provider=eks_cluster.aws_provider)
+                                    )
 
 # Export values to use elsewhere
 pulumi.export("kubeconfig", eks_cluster.kubeconfig)
--- web_server/app/app.py
+++ web_server/app/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+config_color = os.environ.get("CONFIG_COLOR")
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"<p>Hello, {config_color} world!</p>"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
